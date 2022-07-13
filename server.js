2106390ad793009c7adc5829a1fa416f2323a8ad
Heraldo Martinez <mheraldo760@gmail.com>
2022-07-13 13:29:49 -0700

    adding port number

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
+    port : 5432,
     user : 'postgres',
     password : 'root',
     database : 'smart-brain'
@@ -27,7 +28,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('success') })
+app.get('/', (req, res) => {
+	res.send('success');
+})
 
 // checks if credentials match info from database
 app.post('/signin', (req, res) => signin.handleSignin(req, res, db))
