b8398bc0ffacff536930c23bf8dc28c2f06807b9
Heraldo Martinez <mheraldo760@gmail.com>
2022-07-13 13:36:07 -0700

    adding changes

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 /*app.listen(3000, () => {
 	console.log('app is running on port 3000');
 });*/
-
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT;
+app.listen(PORT || 3000, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
 });
 
