ddae52c012086338769653b063263ff101e4a54b
Heraldo Martinez <mheraldo760@gmail.com>
2022-07-13 12:29:08 -0700

    adding changes for controllers

--- server.js
+++ server.js
@@ -1,94 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'root',
+    database : 'smart-brain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'band',
-			entries: 0,
-			joined: new Date()
-		}
-
-	]
-}
-
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.send('success');
 })
 
 // checks if credentials match info from database
-app.post('/signin', (req,res) => {
-	if (req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) {
-		res.json('success');
-	}
-	else {
-		res.status(400).json('error logging in');
-	}
-})
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db))
 
 // checks to see if they can register
-app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		password: password,
-		entries: 0,
-		joined: new Date()
-	})
-	res.json(database.users[database.users.length - 1]);
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db) })
 
 // get the profile of current user
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	let found = false;
-
-	database.users.forEach(user => {
-		if(user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-	if(!found) {
-		res.status(400).json('not found');
-	}
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
 // update the amount of images posted
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	let found = false;
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-	database.users.forEach(user => {
-		if(user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	})
-	if(!found) {
-		res.status(400).json('not found');
-	}
-})
+// update the amount of images posted
+/*app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })*/
 
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
@@ -101,3 +58,4 @@ app.listen(3000, () => {
 /profile/:userID --> GET = user
 /image --> PUT --> user
 */
+
